e6b7f18c2bbf63f1ee4e2db69f070415c119ed4d
Yorick-Lok <yealok2007@gmail.com>
2025-09-30 13:52:16 +0200

    Updated main.py
    Added the calculations of the bottom dice, and the result of adding those numbers up.
    
    Also added a secret message that only shows up if you rolled 6 at least 3 times.

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ How to use:
 1) Run the File
 2) Choose your amount of dice.
 3) The program will now roll a number ranging from 1 to 6, it does this until it has reached the given amount of rolls.
+<<<<<<< Updated upstream
 4) The program will now add all the rolled numbers up, and give you the result.  
+=======
+4) The program will now add all the rolled numbers up, and give you the result.  
+>>>>>>> Stashed changes
--- main.py
+++ main.py
@@ -3,50 +3,74 @@ from random import randint
 def dice_amount():
     """Ask how many dice the user wants to roll. Must be between 3 and 5."""
     while True:
-        # Ask for a number ranging from 3 to 5.
         try:
             amount_input = input("""How many dice would you like to roll? (3-5)
 Type q to quit.\n""")
             if amount_input == 'q':
-                exit() # Quit if the user sends Q
+                exit()  # Quit if the user sends 'q'
             else:
-                amount = int(amount_input) # Turn the user input into an integer.
+                amount = int(amount_input)  # Turn the user input into an integer.
             if 3 <= amount <= 5:
                 print("You entered:", amount)
-                return amount # Return the integer.
-            else: # Return an error.
+                return amount  # Return the integer.
+            else:
                 print("Invalid Input. Please enter a number between 3 and 5.")
-        except ValueError: # Return an error.
+        except ValueError:
             print("Invalid input. Please enter a number.")
 
 def dice_roll(num_dice):
     """Rolls 'num_dice' dice and prints the results."""
     print(f"\nRolling {num_dice} dice...")
     results = []
+    bottom_results = []
+    six_rolls = 0  # Move this outside the loop
+
     for i in range(1, num_dice + 1):
         rolled_number = randint(1, 6)
-        print(f"Dice {i}: {rolled_number}")
-        results.append(rolled_number) # Print a randomly generated number ranging from 1 to 6.
-    return results
+        results.append(rolled_number)
 
-def total_count(rolls):
+        bottom_rolled_number = 7 - rolled_number
+        bottom_results.append(bottom_rolled_number)
+
+        print(f"Dice {i}: {rolled_number}")
+        print(f"Bottom of Dice {i}: {bottom_rolled_number}")
+
+        if rolled_number == 6:
+            six_rolls += 1
+
+    # After all dice are rolled, check for 3 or more sixes
+    if six_rolls >= 3:
+        print(f"\nYou rolled a six {six_rolls} times!")
+
+    return results, bottom_results
+
+
+def total_count(rolls, bottom_rolls):
     """Determines the number the dice add up to."""
     print("\nAdding up the rolled numbers...")
     total = sum(rolls)
+    bottom_total = sum(bottom_rolls)
     print(f"Your dice add up to: {total}")
+    print(f"The bottom of your dice add up to: {bottom_total}")
 
 def play_game():
     """Main Game Loop."""
     while True:
         dice_count = dice_amount()
-        rolled_dice = dice_roll(dice_count)
-        total_count(rolled_dice)
+        rolled_dice, bottom_dice = dice_roll(dice_count)
+        total_count(rolled_dice, bottom_dice)
 
-        again = input("Would you like to play again? (y/n)\n")
-        if again != 'y':
-            print("Thanks for playing!")
-            break
+        while True:
+            again = input("Would you like to play again? (y/n)\n")
+            if again == 'y':
+                print("\n" + "-" * 30 + "\n")
+                break  # Break the inner loop and start a new game
+            elif again == 'n':
+                print("Thanks for playing!")
+                return  # Exit the function, ending the game
+            else:
+                print("Invalid input. Please enter 'y' or 'n'.")
+
 
 # Run the game
-if __name__ == "__main__":
-    play_game()
+play_game()
